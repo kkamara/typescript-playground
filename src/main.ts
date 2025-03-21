1e14a703713378cb5780e15760a0bc185258fc19
kkamara <kamaracomputers@gmail.com>
2025-03-21 08:45:38 +0000

    Add more code to main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ class Test {}
 type Arr = Array<string>;
 type Num = Number;
 type Str = string;
+enum EnumType{ apple, pear, };
+type BigNum = bigint;
+type VoidType = void;
+type NeverType = never;
+type UnknownType = unknown;
+type TupleType = [string, number];
+type NullType = null;
 
 const run = () => {
     let obj: ObjType = {};
@@ -38,6 +45,28 @@ const run = () => {
 
     const myString: Str = undefined;
     console.log("myString", myString);
+
+    const myEnum: EnumType = undefined;
+    console.log("myEnum", myEnum);
+
+    const myBigNum: BigNum = undefined;
+    console.log("myBigNum", myBigNum);
+
+    const myVoid: VoidType = undefined;
+    console.log("myVoid", myVoid);
+
+    // This throws an error.
+    // const myNever: NeverType = undefined;
+    // console.log("myNever", myNever);
+
+    const myUnknown: UnknownType = undefined;
+    console.log("myUnknown", myUnknown);
+
+    const myTuple: TupleType = undefined;
+    console.log("myTuple", myTuple);
+
+    const myNull: NullType = undefined;
+    console.log("myNull", myNull);
 };
 
 run();
