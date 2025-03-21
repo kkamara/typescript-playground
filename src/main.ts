903eaf6cdcc0140e500aad6f9029498d62a5e04e
kkamara <kamaracomputers@gmail.com>
2025-03-21 08:25:29 +0000

    Add more code to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,29 @@ type ObjType = {
         test: string;
     };
 };
+type bool = true|false;
+type func = () => {};
+class Test {}
 
 const run = () => {
     let obj: ObjType = {};
-
     if (obj.response?.test) {
         console.log("response there");
     } else {
         console.log("response not there");
     }
+
+    const myBool: bool = undefined;
+    console.log("myBool", myBool);
+
+    const myFunc: func = undefined;
+    console.log("myFunc", myFunc);
+
+    const myClass: Test = undefined;
+    console.log("myClass", myClass);
+
+    const myNewClass: Test = new Test();
+    console.log("myNewClass", myNewClass);
 };
 
 run();
