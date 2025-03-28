5b78b7f27f1ed09f41021639f19f10ddab0a7112
kkamara <kamaracomputers@gmail.com>
2025-03-28 02:25:07 +0000

    Send an HTTP request in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,33 @@
-type ObjType = {
-    response?: {
-        test: string;
-    };
-};
-type Bool = true|false;
-type Func = () => {};
-class Test {}
-type Arr = Array<string>;
-type Num = Number;
-type Str = string;
-enum EnumType{ apple, pear, };
-type BigNum = bigint;
-type VoidType = void;
-type NeverType = never;
-type UnknownType = unknown;
-type TupleType = [string, number];
-type NullType = null;
+import axios from "axios";
 
-const run = () => {
-    let obj: ObjType = {};
-    if (obj.response?.test) {
-        console.log("response there");
-    } else {
-        console.log("response not there");
+interface HttpResponse<T> {
+    data: T;
+}
+
+interface SuccessBody {
+    message?: string;
+}
+
+interface ErrorBody {
+    message?: string;
+}
+
+const run = async () => {
+    try {
+        await axios.get<HttpResponse<SuccessBody>>("http://localhost:3000/api/success")
+            .then(res => {
+                console.log(res.data.data.message);
+            })
+            .catch(err => {
+                throw err;
+            });
+    } catch (err) {
+        if (axios.isAxiosError<ErrorBody>(err)) {
+            console.log(err.response.data.message);
+        } else {
+            console.log(err.message);
+        }
     }
-
-    const myBool: Bool = undefined;
-    console.log("myBool", myBool);
-
-    const myFunc: Func = undefined;
-    console.log("myFunc", myFunc);
-
-    const myClass: Test = undefined;
-    console.log("myClass", myClass);
-
-    const myNewClass: Test = new Test();
-    console.log("myNewClass", myNewClass);
-
-    const myArray: Arr = undefined;
-    console.log("myArray", myArray);
-
-    const myNumber: Num = undefined;
-    console.log("myNumber", myNumber);
-
-    const myString: Str = undefined;
-    console.log("myString", myString);
-
-    const myEnum: EnumType = undefined;
-    console.log("myEnum", myEnum);
-
-    const myBigNum: BigNum = undefined;
-    console.log("myBigNum", myBigNum);
-
-    const myVoid: VoidType = undefined;
-    console.log("myVoid", myVoid);
-
-    // This throws an error.
-    // const myNever: NeverType = undefined;
-    // console.log("myNever", myNever);
-
-    const myUnknown: UnknownType = undefined;
-    console.log("myUnknown", myUnknown);
-
-    const myTuple: TupleType = undefined;
-    console.log("myTuple", myTuple);
-
-    const myNull: NullType = undefined;
-    console.log("myNull", myNull);
 };
 
 run();
