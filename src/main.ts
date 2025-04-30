4b9160ceb60a118e887469fdbeb2fb3549dee0d4
kkamara <kamaracomputers@gmail.com>
2025-04-30 15:26:15 +0100

    Update indentation to 2 spaces

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 import axios from "axios";
 
 interface HttpResponse<T> {
-    data?: T;
+  data?: T;
 }
 
 interface SuccessBody {
-    message?: string;
+  message?: string;
 }
 
 interface ErrorBody {
-    message?: string;
+  message?: string;
 }
 
 const run = async () => {
-    await axios.get<HttpResponse<SuccessBody>>("http://localhost:3000/api/v1/success")
-        .then(res => {
-            console.log("Result:", res.data.data?.message);
-        })
-        .catch((err: Error) => {
-            if (axios.isAxiosError<ErrorBody>(err)) {
-                console.log("HTTP error:", err.response?.data.message);
-            } else {
-                console.log("Error:", err.message);
-            }
-        });
+  await axios.get<HttpResponse<SuccessBody>>("http://localhost:3000/api/v1/success")
+    .then(res => {
+      console.log("Result:", res.data.data?.message);
+    })
+    .catch((err: Error) => {
+      if (axios.isAxiosError<ErrorBody>(err)) {
+        console.log("HTTP error:", err.response?.data.message);
+      } else {
+        console.log("Error:", err.message);
+      }
+    });
 };
 
 run();
